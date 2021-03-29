8dc568bfd0a33e2218478adae04dc7cb13ef6ceb
Mahima <mahimapatel54@gmail.com>
2021-03-29 19:01:33 +0530

    Initial commit

---
+++ lib/minerva_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "minerva_view_tool/version"
+require "minerva_view_tool/renderer"
+
+module MinervaViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
